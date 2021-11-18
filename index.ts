29823b82d34133e66afb7e6cbfe5256cc08e8e5a
luisagcastro31 <luisagcastro312@gmail.com>
2021-11-18 00:23:43 -0500

    Buscar usuarios

--- index.ts
+++ index.ts
@@ -4,16 +4,24 @@ import UserModel from "./models/user";
 const main = async () => {
     await conectarBD();
 
-    UserModel.create({
+    // CREAR UN USUARIO    
+/*     UserModel.create({
         correo:"ola@onlanguagechange.com",
         identificacion: "8736r",
         nombre: "Luisa",
         apellido: "Castro",
     }).then((u) => {
-        console.log("Usuario Creado", u);
+        console.log("Usuario creado", u);
     }).catch(e =>{
         console.error("Error");
-    })
+    }); */
+
+    //OBTENER LOS USUARIOS
+    await UserModel.find().then((u)=> {
+        console.log("usuarios", u);
+    }).catch(e => {
+        console.error("error", e)
+    });
 };
 
 main();
