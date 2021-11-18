5eaf75258676b05d42638a6f44af27232fb155dd
luisagcastro31 <luisagcastro312@gmail.com>
2021-11-18 00:14:34 -0500

    Primer usuario creado

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
 import conectarBD from "./db/db";
+import UserModel from "./models/user";
 
-const main = async ()=>{
+const main = async () => {
     await conectarBD();
+
+    UserModel.create({
+        correo:"ola@onlanguagechange.com",
+        identificacion: "8736r",
+        nombre: "Luisa",
+        apellido: "Castro",
+    }).then((u) => {
+        console.log("Usuario Creado", u);
+    }).catch(e =>{
+        console.error("Error");
+    })
 };
 
 main();
