bc981177e6db31d018d39780407190ea16f4971a
luisagcastro31 <90797895+luisagcastro31@users.noreply.github.com>
2021-12-10 21:56:02 -0500

    Crear carpeta de objetivos

---
+++ models/objetive.ts
@@ -0,0 +1,29 @@
+import {Schema, model} from 'mongoose';
+import {Enum_TipoObjetivo} from './enums';
+import {ProyectModel} from './proyect';
+
+interface Objective{
+    descripcion: string,
+    tipo: Enum_TipoObjetivo,
+    proyecto: Schema.Types.ObjectId;
+}
+
+const objetiveSchema = new Schema<Objective>({
+    descripcion:{
+        type: String,
+        required: true,
+    },
+    tipo:{
+        type: String,
+        enum: Enum_TipoObjetivo,
+        required: true,
+    },
+    proyecto: {
+        type: Schema.Types.ObjectId,
+        ref: ProyectModel,
+    }
+});
+
+const ObjectiveModel = model("Objetivo", objetiveSchema);
+
+export {ObjectiveModel};
